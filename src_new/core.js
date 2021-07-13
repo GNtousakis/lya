6971b0c37f3985b44f99c49de80164908343e4c3
GNtousakis <greco1996@gmail.com>
2021-07-13 02:54:26 -0400

    Create prologue string

--- src_new/core.js
+++ src_new/core.js
@@ -1,7 +1,13 @@
 const proxy = require('./wrappers/proxy.js');
 const modJs = require('./import/modify.js');
+const prologue = require('./global/prologue.js');
 
 const start = (config) => {    
+    /* Creates the prologue string and clones
+    *  the global object values in order to be imported
+    */ 
+    prologue.create(config);
+
     /* Replace the original nodejs import functions calls
     *  with the modified ones we created
     */
--- src_new/global/prologue.js
+++ src_new/global/prologue.js
@@ -19,21 +19,61 @@ const cloneGlobals = (keys) => {
 
 }
 
-/* Skip any user requested keys
+/* Skip any user requested keys from 
 *
 */
 const skip = (keys, exclude) => {
     return keys;
 };
 
-/* Create the prologue of each module import 
+/* Read the keys from the default-names.json
+*  file and skip any user requested keys
 *
 */
-const createPrologueString = (env) => {
+const getKeys = (env) => {
     let keys = require(keysPath);
     keys = skip(keys, env);
+    
+    return keys;
+}
+
+/* Create the string that we import on all
+*  imported modules
+*
+*  @json: the required json object that holds 
+*  the key values and names
+*  @return: the prologue string 
+*/
+const createPrologueString = (json) => {
+    let prologue = '';
+    for (const category in json) {
+        for (const number in json[category]) {
+            const name = json[category][number];
+            prologue += 'var ' + name + ' = localGlobal["' +
+                name + '"];\n';
+        };
+    };
+
+    /* Add the global object to the
+    *  computed string
+    */
+    prologue = 'var global = localGlobal["proxyGlobal"]\n' + prologue;
+
+    return prologue;
+}
+
+/* Create the string module imports and 
+*  clone all the functions from the global
+*  object
+*
+*/
+const createPrologue = (env) => {
+    let keys = getKeys(env);
+    let prologueString = createPrologueString(keys);
+
+    return prologueString;
 };
 
 module.exports = {
-    create: createPrologueString,
+    create: createPrologue,
 }
